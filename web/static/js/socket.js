b408b172c01b50615b948493fba85732f5748e22
Junerey Casuga <junerey.casuga@gmail.com>
2016-11-10 23:48:18 +0700

    clean up

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -60,19 +60,19 @@ let message = $('#message');
 let name    = $('#name');
 
 message.on('keypress', event => {
-    console.log('test');
-      if (event.keyCode == 13) {
-              channel.push('new_message', { name: name.val(), message: message.val() });
-              message.val('');
-            }
+  if (event.keyCode == 13) {
+    channel.push('new_message', { name: name.val(), message: message.val() });
+    message.val('');
+  }
 });
 
 channel.on('new_message', payload => {
-      list.append(`<b>${payload.name || 'Anonymous'}:</b> ${payload.message}<br>`);
-      list.prop({scrollTop: list.prop("scrollHeight")});
+    list.append(`<b>${payload.name || 'Anonymous'}:</b> ${payload.message}<br>`);
+    list.prop({scrollTop: list.prop("scrollHeight")});
 });
 
 channel.join()
   .receive("ok", resp => { console.log("Joined successfully", resp) })
   .receive("error", resp => { console.log("Unable to join", resp) })
+
 export default socket
