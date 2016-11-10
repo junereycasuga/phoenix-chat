339173cd8953c297e135cc33a488b185df1ad3ba
Junerey Casuga <junerey.casuga@gmail.com>
2016-11-11 00:35:59 +0700

    clean

--- web/channels/lobby_channel.ex
+++ web/channels/lobby_channel.ex
@@ -1,13 +1,13 @@
 defmodule Chatroom.LobbyChannel do
-    use Phoenix.Channel
+  use Phoenix.Channel
 
-    def join("lobby", _payload, socket) do
-        {:ok, socket}
-    end
+  def join("lobby", _payload, socket) do
+    {:ok, socket}
+  end
 
-    def handle_in("new_message", payload, socket) do
-        broadcast! socket, "new_message", payload
-        {:noreply, socket}
-    end
+  def handle_in("new_message", payload, socket) do
+    broadcast! socket, "new_message", payload
+    {:noreply, socket}
+  end
 end
 
